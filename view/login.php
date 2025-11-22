7c5f220b93e9daacb6254c64373240e8031dedbe
crybaby050 <benthiam750@gmail.com>
2025-11-22 18:13:31 +0000

    finalisation du visuel de la page de connection

--- view/login.php
+++ view/login.php
@@ -1,4 +1,23 @@
 <div class="logContain">
     <div class="logimg"></div>
-    <div class="logform"></div>
+    <div class="logform">
+        <form action="" method="post">
+            <div class="h1">
+                <img src="/../image/school logo.jpg" alt="">
+            </div>
+            <div>
+                <label for="">Mail</label>
+                <input type="text" placeholder="Ex: monmail@gmail.com" name="mail">
+                <i class="fa-solid fa-envelope"></i>
+                <p><?= $error ?? '' ?></p>
+            </div>
+            <div>
+                <label for="">Password</label>
+                <input type="password" placeholder="mon mot de passe" name="mdp">
+                <i class="fa-solid fa-eye"></i>
+                <p><?= $error ?? '' ?></p>
+            </div>
+            <button type="submit" name="log" >Se Connecter</button>
+        </form>
+    </div>
 </div>
