2a9381bd1540b52c61f84ea6b633a476d2771ee7
crybaby050 <benthiam750@gmail.com>
2025-11-22 12:34:44 +0000

    debut de la configuration du visuel de la page de connexion du header et du css

---
+++ other/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/other/header.css">
+    <title>Ecole 221</title>
+</head>
+<body>
+    
---
+++ view/login.php
@@ -0,0 +1,4 @@
+<div class="logContain">
+    <div class="logimg"></div>
+    <div class="logform"></div>
+</div>
