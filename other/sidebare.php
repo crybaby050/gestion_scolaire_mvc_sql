875a0352ae1750a0fbdf6909a6b126941db83610
crybaby050 <benthiam750@gmail.com>
2025-11-23 15:17:30 +0000

    debut de la configuration visuel du sidebare et du header

---
+++ other/sidebare.php
@@ -0,0 +1,24 @@
+<input type="checkbox" id="toggle">
+<header class="header">
+    <label for="toggle" class="menu-btn">☰</label>
+    <h2>Mon Application PHP</h2>
+</header>
+
+<aside class="sidebar">
+    <div class="sidebar-header">
+        <div>
+            <img src="/../image/school logo.jpg" alt="">
+        </div>
+        <h2>ECOLE 221</h2>
+    </div>
+    <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Page 1</a></li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Déconnexion</a></li>
+    </ul>
+</aside>
+
+<div class="content">
+    <h1>Contenu</h1>
+</div>
