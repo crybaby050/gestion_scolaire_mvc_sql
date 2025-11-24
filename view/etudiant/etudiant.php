656d87e45e0eb4c631f9bd5b68912b9eccd3d0c2
crybaby050 <benthiam750@gmail.com>
2025-11-24 19:17:08 +0000

    correction du bug au niveau du lien d'appel du controller

--- view/etudiant/etudiant.php
+++ view/etudiant/etudiant.php
@@ -1,5 +1,5 @@
 <?php 
-    require_once __DIR__ . '/../../other/init.php';
+    require_once __DIR__ . '/../../controller/etudiantController.php';
     $etudiants = affiheEtudiant();
     $classes = getAllClasse();
 ?>
