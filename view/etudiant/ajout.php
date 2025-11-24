4174b5b8cb535bb363c96d27c0e12313b1f7fae9
crybaby050 <benthiam750@gmail.com>
2025-11-24 15:58:34 +0000

    debut de la configuration du visuel de la page ajout etudiant

--- view/etudiant/ajout.php
+++ view/etudiant/ajout.php
@@ -1,3 +1,28 @@
-<div class="content">
-    
+<!-- <div class="content"> -->
+    <div class="add-container">
+    <h2>Ajouter un étudiant</h2>
+    <form class="form-card">
+        <label>Nom</label>
+        <input type="text" placeholder="Ex: Thiam" name="nom">
+        <label>Nom prenom</label>
+        <input type="text" placeholder="Ex: Jean" name="pre">
+        <label>Email</label>
+        <input type="text" placeholder="exemple@mail.com" name="mail">
+        <label>Téléphone</label>
+        <input type="text" placeholder="77xxxxxxx" name="tel">
+        <label>Classe</label>
+        <select name="cls">
+            <option value="dev">Développement</option>
+            <option value="réseau">Réseau</option>
+            <option value="design">Design</option>
+        </select>
+        <label>Adresse</label>
+        <input type="text" placeholder="Ex: Dakar, Yoff" name="ads">
+        <div class="btns">
+            <button type="button" class="cancel">Annuler</button>
+            <button type="submit" class="save" name="add-etu">Enregistrer</button>
+        </div>
+    </form>
 </div>
+
+<!-- </div> -->
