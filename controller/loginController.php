b778129824acbe50044a855e2cb81ec6d3103fa5
crybaby050 <benthiam750@gmail.com>
2025-11-24 23:59:21 +0000

    update du login

--- controller/loginController.php
+++ controller/loginController.php
@@ -6,4 +6,5 @@ function loginPage()
     if (isset($_POST['log'])) {
         $errors = loginUser($_POST['mail'], $_POST['mdp']);
     }
+    require_once __DIR__ . '/../view/login.php';
 }
--- view/login.php
+++ view/login.php
@@ -1,24 +1,38 @@
-<div class="logContain">
-    <div class="logimg"></div>
-    <div class="logform">
-        <form action="" method="post">
-            <div class="h1">
-                <img src="/../image/school logo.jpg" alt="">
-            </div>
-            <div>
-                <label for="">Mail</label>
-                <input type="text" placeholder="Ex: monmail@gmail.com" name="mail">
-                <i class="fa-solid fa-envelope"></i>
-                <p><?= $errors['mail'] ?? '' ?></p>
-            </div>
-            <div>
-                <label for="">Password</label>
-                <input type="password" placeholder="mon mot de passe" name="mdp">
-                <i class="fa-solid fa-eye"></i>
-                <p><?= $errors['mdp'] ?? '' ?></p>
-            </div>
-            <?= $errors['verif'] ?? '' ?>
-            <button type="submit" name="log" >Se Connecter</button>
-        </form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/../../other/login.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="logContain">
+        <div class="logimg"></div>
+        <div class="logform">
+            <form action="" method="post">
+                <div class="h1">
+                    <img src="/../image/school logo.jpg" alt="">
+                </div>
+                <div>
+                    <label for="">Mail</label>
+                    <input type="text" placeholder="Ex: monmail@gmail.com" name="mail">
+                    <i class="fa-solid fa-envelope"></i>
+                    <p><?= $errors['mail'] ?? '' ?></p>
+                </div>
+                <div>
+                    <label for="">Password</label>
+                    <input type="password" placeholder="mon mot de passe" name="mdp">
+                    <i class="fa-solid fa-eye"></i>
+                    <p><?= $errors['mdp'] ?? '' ?></p>
+                </div>
+                <?= $errors['verif'] ?? '' ?>
+                <button type="submit" name="log">Se Connecter</button>
+            </form>
+        </div>
     </div>
-</div>
+</body>
+
+</html>
