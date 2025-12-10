27a5fd53667748464aeb54da3369a6732195b812
crybaby050 <benthiam750@gmail.com>
2025-12-10 20:39:37 +0000

    Ajout d'un alter table au niveau de la base de donnée coté classe pour la supression des filiere qui entraine une supression en chaine,creation de fonction d'ajout filiere ainsi que de fonction de supression de filiere

--- model/filiereModel.php
+++ model/filiereModel.php
@@ -1,1 +1,15 @@
 <?php
+function ajoutFiliere($lib){
+    $pdo = getPDO();
+    $sql = "INSERT INTO 
+    filiere (libelle) 
+    VALUES(:lib)";
+    $stmt = $pdo->prepare($sql);
+    $stmt -> execute(['lib'=>$lib]);
+}
+function deleteFiliere($id){
+    $pdo = getPDO();
+    $sql = "DELETE FROM filiere WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['id'=>$id]);
+}
