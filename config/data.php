24f5cfccb202df5bdab9a8703f3c37954e0dd1cd
crybaby05 <benthiam750@gmail.com>
2026-03-05 13:23:25 +0000

    changement de la base de donnée

--- config/data.php
+++ config/data.php
@@ -2,9 +2,9 @@
 function getPDO(){
     try{
         return new PDO(
-            "mysql:host=127.0.0.1;dbname=ecole;charset=utf8;port=3307",
+            "mysql:host=127.0.0.1;dbname=ecole;charset=utf8;port=3306",
             "root",
-            "seydinathiam05",
+            "",
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
